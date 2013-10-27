5503aa811a88ad9791f3de76d400073e63e41fd6
Benjamin Winkler <abwinkler999@gmail.com>
2013-10-26 23:36:44 -0400

    Refactoring item tests

---
+++ lib/store.rb
@@ -0,0 +1,8 @@
+class Store
+	attr_accessor :day
+
+	def initialize
+		@day = 0
+	end
+	
+end
--- spec/product_spec.rb
+++ spec/product_spec.rb
@@ -1,33 +1,33 @@
 require 'product'
-
+require 'store'
 
 
 describe Product do 
 
 	before(:each) do
-		item = Product.new
-		item.original_price = 100
+		@item = Product.new
+		@item.original_price = 100
 	end
 
 	it "can discount an object by 10%" do
-		item.validate_discount(0.10)
-		item.current_price.should == 90
+		@item.validate_discount(0.10)
+		@item.current_price.should == 90
 	end
 
 	it "can't discount an object by 1%" do
-		item.validate_discount(0.01)
-		item.current_price.should == 100
+		@item.validate_discount(0.01)
+		@item.current_price.should == 100
 	end
 
 	it "can't discount an object by 50%" do
-		item.validate_discount(0.50)
-		item.current_price.should == 100
+		@item.validate_discount(0.50)
+		@item.current_price.should == 100
 	end
 
 	it "can't run a promotion longer than 30 days" do
-		item.validate_discount(0.10)
-		item.advance_clock(60)
-		item.current_price.should == 100
+		@item.validate_discount(0.10)
+		@item.advance_clock(60)
+		@item.current_price.should == 100
 	end
 end
 
